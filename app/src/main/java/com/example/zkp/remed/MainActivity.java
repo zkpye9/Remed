4b3d2ef253a479967175dba2cefb7ec39633a582
zkpye9 <kunpengzhang@email.arizona.edu>
2016-01-23 00:36:02 -0700

    parse signUp

--- app/src/main/java/com/example/zkp/remed/MainActivity.java
+++ app/src/main/java/com/example/zkp/remed/MainActivity.java
@@ -27,25 +27,6 @@ public class MainActivity extends AppCompatActivity {
 
         Parse.initialize(this, "4ZnAIecM4dhXJ1iqM4KtdoZHPkyOG415vACmyMgb", "0hCpbdIilElJcZGj8NUBln85ekiFO1xI2LEpqVEi");
 
-        /*ParseUser user = new ParseUser();
-        user.setUsername("my name");
-        user.setPassword("my pass");
-        user.setEmail("email@example.com");
-
-        // other fields can be set just like with ParseObject
-        user.put("phone", "650-555-0000");
-
-        user.signUpInBackground(new SignUpCallback() {
-            public void done(ParseException e) {
-                if (e == null) {
-                    // Hooray! Let them use the app now.
-                } else {
-                    // Sign up didn't succeed. Look at the ParseException
-                    // to figure out what went wrong
-                }
-            }
-        });*/
-
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
