c33afcf1dc3e6129fe46bc660f2d2dce3862a907
Mike <mfinley3@email.arizona.edu>
2016-01-22 21:42:39 -0700

    Added support for login buttons to be created on start of loginActivity. Still will not display, soon though!

--- app/src/main/java/com/example/zkp/remed/LoginActivity.java
+++ app/src/main/java/com/example/zkp/remed/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.zkp.remed;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,7 +16,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.fragment_container, new LoginButtons());
+        ft.commit();
     }
-
 }
---
+++ app/src/main/java/com/example/zkp/remed/LoginButtons.java
@@ -0,0 +1,20 @@
+package com.example.zkp.remed;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mike on 1/22/2016.
+ */
+
+public class LoginButtons extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_login_buttons, container, false);
+        return view;
+    }
+}
