03a54d561731487e4bb1ce59a7867b5a46e1411e
zkpye9 <kunpengzhang@email.arizona.edu>
2016-01-23 21:15:20 -0700

    part of the new Med (backend)

--- app/src/main/java/com/example/zkp/remed/NewMedActivity.java
+++ app/src/main/java/com/example/zkp/remed/NewMedActivity.java
@@ -6,8 +6,28 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.example.zkp.remed.model.Record;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
 
 public class NewMedActivity extends AppCompatActivity {
+
+    private EditText medName;
+    private String medNameParse;
+    private EditText hour;
+    private String hourParse;
+    private EditText min;
+    private String minParse;
+    private RadioGroup APM;
+
+    private RadioGroup Frequency;
+
+    private Record newRecord;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +40,17 @@ public class NewMedActivity extends AppCompatActivity {
     }
 
     public void create(View view) {
+        medName = (EditText)findViewById(R.id.medName);
+        medNameParse = medName.getText().toString();
 
+        hour = (EditText)findViewById(R.id.Hour);
+        hourParse = hour.getText().toString();
+
+        min = (EditText)findViewById(R.id.Min);
+        minParse = min.getText().toString();
+
+        newRecord = new Record(medNameParse, hourParse, minParse);
+        ((ArrayList<Record>)ParseUser.getCurrentUser().get("record")).add(newRecord);
     }
 
 }
